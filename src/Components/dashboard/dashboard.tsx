2cce7bbf9c20f87a99163ad7597d13fec1106499
mchauveni <matheo.chaumet.venier@gmail.com>
2023-12-11 14:11:45 +0100

    reworked dashboard element because context was deleted

--- src/Components/dashboard/dashboard.tsx
+++ src/Components/dashboard/dashboard.tsx
@@ -1,13 +1,46 @@
-import { useContext } from "react";
-import Cooldown from "../cooldown/cooldown";
+import { useContext, useEffect, useState } from "react";
+import CooldownComponent from "../cooldown/cooldown";
 import { DashboardContext } from "../../Context/dashboard/DashboardContext";
 
 import "./dashboard.css";
+import { Cargo, Cooldown, Fuel, Nav, ShipData } from "../../Models/ShipInterface";
+import { Waypoint } from "../../Models/WaypointInterface";
+import { Market } from "../../Models/MarketInterface";
+import SpaceTraders from "../../SpaceTraders";
+import { useParams } from "react-router-dom";
 
 const Dashboard = () => {
+    const { shipSymbol } = useParams();
+
+    const [ship, setShip] = useState<ShipData | null>()
+    const [waypoint, setWaypoint] = useState<Waypoint | null>()
+    const [market, setMarket] = useState<Market | null>()
+
+    const [cargo, setCargo] = useState<Cargo | null>(null);
+    const [nav, setNav] = useState<Nav | null>(null);
+    const [cooldown, setCooldown] = useState<Cooldown | null>(null);
+    const [fuel, setFuel] = useState<Fuel | null>(null);
+
+    useEffect(() => {
+        if (ship) {
+            setCargo(ship.cargo);
+            setNav(ship.nav);
+            setCooldown(ship.cooldown);
+            setFuel(ship.fuel);
+        }
+    }, [ship])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setShip(await SpaceTraders.getShip(shipSymbol))
+        }
+
+        fetchData();
+    }, [])
+
     return (
         <div className="dashboard">
-            <Cooldown></Cooldown>
+            <CooldownComponent cooldown={cooldown}></CooldownComponent>
         </div>
     )
 }
