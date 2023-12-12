4e6c5fc9f9e53f10d983193d5a781c474f564a20
mchauveni <matheo.chaumet.venier@gmail.com>
2023-12-12 18:39:46 +0100

    added jest and dependencies for TS.

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  }
+};
+
+export default config;
