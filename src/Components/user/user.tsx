11b2e0dadcdc79ad1685ca54b6aa518e6516c5cd
mchauveni <matheo.chaumet.venier@gmail.com>
2023-12-13 15:57:25 +0100

    changed user styling

--- src/Components/user/user.tsx
+++ src/Components/user/user.tsx
@@ -1,6 +1,9 @@
 import { AgentData } from '../../Models/AgentInterface';
 import "./user.css";
-import credits from "../../assets/icons/credits.svg";
+import hqIcon from "../../assets/icons/headquarters.svg";
+import factionIcon from "../../assets/icons/faction.svg";
+import creditIcon from "../../assets/icons/credits.svg";
+import fleetIcon from "../../assets/icons/ship.svg";
 
 interface UserComponentProps {
 	user: AgentData | null;
@@ -13,12 +16,22 @@ const User: React.FC<UserComponentProps> = ({ user }) => {
 				<div className='user'>
 					<h3 className='user__symbol'>{user.symbol}</h3>
 					<div className='user__infos'>
-						<span className='user__headquarters'>HQ : {user.headquarters}</span>
-						<span className='user__faction'>Faction : {user.startingFaction}</span>
-					</div>
-					<div className='user__creditsWrapper'>
-						<img className='user__creditsIcon' src={credits} alt="credits" />
-						<span className='user__credits'>{user.credits}</span>
+						<div className='user__creditsWrapper'>
+							<img className='user__creditIcon' src={creditIcon} alt="credits" />
+							<span className='user__credits'>{user.credits}</span>
+						</div>
+						<div className='user__fleetWrapper'>
+							<img className='user__fleetIcon' src={fleetIcon} alt="credits" />
+							<span className='user__shipCount'>{user.shipCount}</span>
+						</div>
+						<div className='user__factionWrapper'>
+							<img className='user__factionIcon' src={factionIcon} alt="credits" />
+							<span className='user__faction'>{user.startingFaction}</span>
+						</div>
+						<div className='user__hqWrapper'>
+							<img className='user__hqIcon' src={hqIcon} alt="credits" />
+							<span className='user__headquarters'>{user.headquarters}</span>
+						</div>
 					</div>
 				</div>
 			) : (
