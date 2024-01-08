34962053b9dc1f8e3b7cf0755c81925121245faf
mchauveni <matheo.chaumet.venier@gmail.com>
2024-01-08 16:53:11 +0100

    basic full fleet interface

--- src/Components/fleet/fleet.tsx
+++ src/Components/fleet/fleet.tsx
@@ -1,30 +1,56 @@
-import { Ship } from "../../Models/ShipInterface";
+import { useContext } from "react";
 import "./fleet.css";
+import { SpacetradersContext } from "../../Context/spacetraders/SpacetradersContext";
+import { Link } from "react-router-dom";
 
-const Fleet = ({ fleet }: { fleet: Ship[] | null }) => {
+import dockedIcon from "/assets/icons/docked.svg";
+import in_orbitIcon from "/assets/icons/in_orbit.svg";
+import in_transitIcon from "/assets/icons/in_transit.svg";
+import { Ship } from "../../Models/ShipInterface";
+
+const Fleet = () => {
+    let STContext = useContext(SpacetradersContext);
+
+    const renderStatusIcon = (ship: Ship) => {
+        switch (ship.nav.status) {
+            case "DOCKED":
+                return <img src={dockedIcon} className="fleet__shipStatus" />;
+            case "IN_ORBIT":
+                return <img src={in_orbitIcon} className="fleet__shipStatus" />;
+            case "IN_TRANSIT":
+                return <img src={in_transitIcon} className="fleet__shipStatus" />;
+        }
+    }
+
     return (
-        <>
-            <table>
-                <thead>
+        <div className="fleet__wrapper">
+            <table className="fleet">
+                <thead className="fleet__head">
                     <tr>
-                        <th>Symbol</th>
-                        <th>Role</th>
-                        <th>Status</th>
-                        <th>Fuel</th>
-                        <th>Cargo</th>
+                        <th className="fleet__cell">Symbol</th>
+                        <th className="fleet__cell">Role</th>
+                        <th className="fleet__cell">Status</th>
+                        <th className="fleet__cell">Fuel</th>
+                        <th className="fleet__cell">Cargo</th>
                     </tr>
                 </thead>
-                <tbody>
-                    {fleet ? (
-                        fleet.map(() => (
-                            <div></div>
+                <tbody className="fleet__body">
+                    {STContext.fleet ? (
+                        STContext.fleet.map((ship) => (
+                            <tr className="fleet__row">
+                                <td className="fleet__cell"><Link to={"/fleet/" + ship.symbol} className="fleet__shipSymbol">{ship.symbol}</Link></td>
+                                <td className="fleet__cell"><span className="fleet__shipRole">{ship.registration.role}</span></td>
+                                <td className="fleet__cell">{renderStatusIcon(ship)}</td>
+                                <td className="fleet__cell"><span className="fleet__shipFuel">{ship.fuel.current} / {ship.fuel.capacity}</span></td>
+                                <td className="fleet__cell"><span className="fleet__shipCargo">{ship.cargo.units} / {ship.cargo.capacity}</span></td>
+                            </tr>
                         ))
                     ) : (
-                        <div className="loader big"></div>
+                        <tr className="loader big"></tr>
                     )}
                 </tbody>
             </table>
-        </>
+        </div>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import LoginPage from './Components/login/Login.tsx';
 import { AuthContextProvider } from './Context/auth/AuthContextProvider.tsx';
 import Dashboard from './Components/dashboard/dashboard.tsx';
 import { SpacetradersProvider } from './Context/spacetraders/SpacetradersContextProvider.tsx';
+import Fleet from './Components/fleet/fleet.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 					<Routes>
 						<Route path="/" element={<App />}>
 							<Route path="/fleet/:shipSymbol" element={<Dashboard />}></Route>
-							<Route path="/fleet" element={<Dashboard />}></Route>
+							<Route path="/fleet" element={<Fleet />}></Route>
 						</Route>
 						<Route path="/login" element={<LoginPage />}></Route>
 					</Routes>
